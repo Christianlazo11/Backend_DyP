59399143e11b7c3235f3d68a666cd17c285ea163
christian lazo <kenishi.cristianxd@gmail.com>
2022-08-01 11:32:57 -0500

    Apload Image Test

--- src/config.js
+++ src/config.js
@@ -6,4 +6,7 @@ export default {
   MONGODB_URI: process.env.MONGODB_URI,
   SECRET: process.env.SECRET_API,
   HOST: process.env.APP_HOST,
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import { methods as productsController } from "../controllers/products.controller";
 const router = Router();
+import cloudinary from "../services/cloudinary.service";
+import upload from "../services/multer.service";
 
 router
   .get("/", productsController.getAllProducts)
   .get("/:id", productsController.getOneProduct)
   .post("/", productsController.createdProduct)
   .put("/:id", productsController.updatedProduct)
-  .delete("/:id", productsController.deletedProduct);
+  .delete("/:id", productsController.deletedProduct)
+  .post("/uploadImage", upload.single("avatar"), (req, res) => {
+    cloudinary.uploader
+      .upload(req.file.path)
+      .then((resp) => {
+        // return resp.url;
+        console.log(resp.url);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    res.send("Uploading Image...");
+  });
 
 export default router;
